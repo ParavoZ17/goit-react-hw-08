8628f067889641a1216b0e1ba6c2018c62518461
Mykola Naumenko <1712nn2012@gmail.com>
2024-08-28 22:24:14 +0200

    Fixed errors

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -55,7 +55,7 @@ export const refreshUser = createAsyncThunk(
     const savedToken = reduxState.auth.token;
 
     setAuthHeader(savedToken);
-    const response = await axios.get("/users/me");
+    const response = await axios.get("/users/current");
 
     return response.data;
   },
